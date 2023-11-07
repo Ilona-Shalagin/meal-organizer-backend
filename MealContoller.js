1dd4f1603bf3a0957dd2d423e6af871175c21ec3
Ilona  Shalagin <ilona.shalagin25@gmail.com>
2023-11-06 19:29:42 -0500

    Add files via upload

---
+++ MealContoller.js
@@ -0,0 +1,24 @@
+const MealModel = require('./MealModel')
+
+module.exports.getMeal = async(req,res) => {
+    const MyMeal = await MealModel.find();
+    res.send(MyMeal)
+}
+
+module.exports.saveMeals = async(req,res) => {
+    const { title } = req.body;
+    MealModel.create({title})
+    .then((data) => {console.log('Meal added')
+     res.send(data)
+})
+}
+module.exports.deleteMeal = async (req,res) => {
+    const {_id } = req.body
+    MealModel.findByIdAndDelete(_id)
+    .then(() => res.send("Deleted a meal")
+)}
+module.exports.editMeal = async(req,res) => {
+    const {_id,title } = req.body
+    MealModel.findByIdAndUpdate(_id,{title})
+    .then (() => res.send("Edited a meal"))
+}
